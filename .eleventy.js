57f2212186cafce83111fbebf1d02ddf0c9714fa
TatianaFokina <fokinatatian@gmail.com>
2023-04-11 21:18:12 +0400

    Удаляет лишние папки

--- .eleventy.js
+++ .eleventy.js
@@ -78,7 +78,7 @@ module.exports = function(eleventyConfig) {
 	return {
 		dir: {
 			input: "src",
-			output: "docs",
+			output: "dist",
 			includes: "_partials",
 			layouts: "templates",
 			data: "data",
