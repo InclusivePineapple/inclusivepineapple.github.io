9d2e02b874fd0aca0adee7de853a2076d12d0240
TatianaFokina <fokinatatian@gmail.com>
2023-04-20 17:40:42 +0400

    Добавляет правильный `description`

--- src/data/site.js
+++ src/data/site.js
@@ -1,7 +1,7 @@
 module.exports = {
 	"siteLink": "https://inclusivepineapple.github.io",
 	"title": "Инклюзивный ананас",
-	"description": "Подскаст о цифровой доступности.",
+	"description": "Ежемесячный подкаст, где мы обсуждаем всё, что связано с цифровой доступностью, и делимся свежими новостями и событиями.",
 	"ogImageLink": "https://inclusivepineapple.github.io/assets/og-image.jpg",
 	"currentYear": new Date().getFullYear(),
 };
