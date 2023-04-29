2d7747ad581cc12b41ce5b76a595ba31951edc6f
Vasiliy Dudin <dudin.vasiliy@gmail.com>
2023-04-29 16:33:38 +0400

    Переиспользует site.js для фида

--- src/data/site.js
+++ src/data/site.js
@@ -4,4 +4,5 @@ module.exports = {
 	"description": "Ежемесячный подкаст, где мы обсуждаем всё, что связано с цифровой доступностью, и делимся свежими новостями и событиями.",
 	"ogImageLink": "https://inclusivepineapple.github.io/assets/og-image.jpg",
 	"currentYear": new Date().getFullYear(),
+	"language": "ru",
 };
