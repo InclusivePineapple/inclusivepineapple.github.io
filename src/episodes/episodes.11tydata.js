cd7d480b9bc10eab6f6a01877f4dcd08b09837ba
monochromer <monochromer@mail.ru>
2023-05-01 16:40:20 +0500

    Автоматизирует вычисление длительности аудио-файла

--- src/episodes/episodes.11tydata.js
+++ src/episodes/episodes.11tydata.js
@@ -1,20 +1,58 @@
 const path = require('node:path');
-const fs = require('node:fs/promises');
+const fs = require('node:fs');
+const musicmetadata = require('musicmetadata');
+
+// получение длительности mp3-файла без чтения всего содержимого файла
+function getAudioFileDuration(filePath) {
+	return new Promise((resolve, reject) => {
+		const fileStream = fs.createReadStream(filePath);
+		fileStream.on('error', reject);
+		musicmetadata(fileStream, { duration: true }, (error, metaData) => {
+			fileStream.close();
+			if (error) {
+				reject(error);
+				return;
+			}
+			resolve(metaData?.duration)
+		})
+	})
+}
 
 module.exports = {
 	tags: 'episode',
 	layout: 'episode.njk',
 	cover: 'cover.jpg',
 	eleventyComputed: {
-		audioSize: async function(data) {
+		audioFilePath: function(data) {
 			const {
 				page: { inputPath },
 				audio
 			} = data;
 			const episodeFolderPath = path.dirname(inputPath);
 			const audioFilePath = path.join(episodeFolderPath, audio);
-			const audioFileSize = (await fs.stat(audioFilePath)).size;
+			return audioFilePath;
+		},
+
+		audioSize: async function(data) {
+			const {
+				audioFilePath
+			} = data;
+			if (!audioFilePath) {
+				return;
+			}
+			const audioFileSize = (await fs.promises.stat(audioFilePath)).size;
 			return audioFileSize;
+		},
+
+		duration: async function(data) {
+			const {
+				audioFilePath
+			} = data;
+			if (!audioFilePath) {
+				return;
+			}
+			const durationInSeconds = Math.round(await getAudioFileDuration(audioFilePath))
+			return durationInSeconds;
 		}
 	}
 }
